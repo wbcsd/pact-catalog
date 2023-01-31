3c85324ffa778bfe0148b0c26baa2df6598dfb6b
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-31 11:47:23 +0000

    chore: WIP build script

---
+++ build-frontend.sh
@@ -0,0 +1,8 @@
+cd frontend
+git init
+git remote add origin git@github.com:sine-fdn/pact-catalog-web-app.git
+# TO DO: replace 'repo-split' by 'main'
+git pull origin repo-split
+# cd ..
+# mv ./app/* frontend
+# rm app
