f25ffe7e706432752f5006ada68e709b8ad10c1f
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-02-01 16:49:31 +0000

    chore: delete test data and shell script

--- build-frontend.sh
+++
@@ -1,9 +0,0 @@
-mkdir frontend
-git clone --branch repo-split git@github.com:sine-fdn/pact-catalog-web-app.git frontend
-cp -r -f ./frontend/app/* ./frontend
-npm install next
-npm install
-npm install --global vercel@latest
-vercel build --cwd frontend
-vercel deploy
-# test
